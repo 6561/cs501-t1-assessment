0d503f368fdc8756c2f46b930987a406620c7e2f
Bartholomew C <bartconsidine@gmail.com>
2021-09-14 23:52:58 -0400

    New Shell script

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: gunicorn project.server.wsgi:app
 heroku ps:scale web=1
-
+release: ./tables.sh
 
---
+++ tables.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export FLASK_APP=project.server
+export APP_SETTINGS="project.server.config.DevelopmentConfig"
+flask db init
+flask db migrate
+flask db upgrade
