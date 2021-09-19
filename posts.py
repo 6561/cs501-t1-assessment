d709319d08ea3545df407566cb3e075309e2042e
Bartholomew C <bartconsidine@gmail.com>
2021-09-19 18:19:37 -0400

    Different test

--- posts.py
+++ posts.py
@@ -2,8 +2,8 @@ import requests
 
 url = 'https://cs501a1-api-heroku.herokuapp.com/auth/register'
 obj = {
-    'email':'coolcath@cheese.com',
-    'password':'my12345678',
+    'email':'whatever@cheese.com',
+    'password':'my123yut45678',
 }
 
 x=requests.post(url, json = obj)
